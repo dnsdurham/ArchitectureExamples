83687ec4833430cb86b7be2483cda783d30ff6c5
Chad Michel <chadmichel@me.com>
2016-07-27 15:14:50 -0500

    encrypt and compress

--- EncryptFileAndCompress/Program.cs
+++ EncryptFileAndCompress/Program.cs
@@ -12,15 +12,17 @@ namespace EncryptFile
 {
     class Program
     {
+        const int bufSize = 100000;
+
         static void Main(string[] args)
         {
-            const int bufSize = 1000000;
-
             var input = @"input.txt";
+
+            var compress = @"working.compress";
+            var decompress = @"output.txt";
+
             var encrypted = @"working.encrypted";
-            var compressed = @"working.compressed";
-            var decompressed = @"working.deencrypted";
-            var decrypted = @"output.txt";
+            var decrypted = @"working.decrypted";
 
             if (File.Exists(input))
                 File.Delete(input);
@@ -31,11 +33,11 @@ namespace EncryptFile
             if (File.Exists(decrypted))
                 File.Delete(decrypted);
 
-            if (File.Exists(compressed))
-                File.Delete(compressed);
+            if (File.Exists(compress))
+                File.Delete(compress);
 
-            if (File.Exists(decompressed))
-                File.Delete(decompressed);
+            if (File.Exists(decompress))
+                File.Delete(decompress);
 
             var size = 1000;
             if (args != null && args.Length > 0)
@@ -43,6 +45,48 @@ namespace EncryptFile
 
             GenerateRandomFile(input, size);
 
+            CompressFile(input, compress);
+            EncryptFile(compress, encrypted);
+            DecryptFile(encrypted, decrypted);
+            DecompressFile(decrypted, decompress);
+
+            Console.WriteLine("press enter to exit");
+            Console.ReadLine();
+
+        }
+
+        private static void CompressFile(string input, string output)
+        {
+            using (var inFile = File.OpenRead(input))
+            using (var outFile = File.Create(output))
+            using (var Compress = new GZipStream(outFile, CompressionMode.Compress))
+            {
+                byte[] buffer = new byte[bufSize];
+                int numRead;
+                while ((numRead = inFile.Read(buffer, 0, buffer.Length)) != 0)
+                {
+                    Compress.Write(buffer, 0, numRead);
+                }
+            }
+        }
+
+        private static void DecompressFile(string input, string output)
+        {
+            using (var inFile = File.OpenRead(input))
+            using (var outFile = File.Create(output))
+            using (var compress = new GZipStream(inFile, CompressionMode.Decompress))
+            {
+                byte[] buffer = new byte[bufSize];
+                int numRead;
+                while ((numRead = compress.Read(buffer, 0, buffer.Length)) != 0)
+                {
+                    outFile.Write(buffer, 0, numRead);
+                }
+            }
+        }
+
+        private static void EncryptFile(string input, string output)
+        {
             // Create an RijndaelManaged object 
             // with the specified key and IV. 
             using (RijndaelManaged rijAlg = new RijndaelManaged())
@@ -53,62 +97,57 @@ namespace EncryptFile
 
                 var sw = new Stopwatch();
 
-                if (!File.Exists(encrypted))
-                {
-
-                    sw.Start();
-
-                    // Create a decrytor to perform the stream transform.
-                    ICryptoTransform encryptor = rijAlg.CreateEncryptor(rijAlg.Key, rijAlg.IV);
-
-                    using (var inputFileStream = File.OpenRead(input))
-                    using (var outputFileStream = File.OpenWrite(encrypted))
-                    using (CryptoStream csEncrypt = new CryptoStream(outputFileStream, encryptor, CryptoStreamMode.Write))
-                    using (var swEncrypt = new StreamWriter(csEncrypt))
-                    {
-                        var buf = new byte[bufSize];
-
-                        int length = 0;
-                        int offset = 0;
-                        int loopCnt = 0;
-
-                        do
-                        {
-                            length = inputFileStream.Read(buf, 0, buf.Length);
-                            //Write all data to the stream.
-                            csEncrypt.Write(buf, 0, length);
-                            offset += length;
-                            loopCnt++;
-                        } while (length > 0);
-                    }
-
-
-                    sw.Stop();
-                    Console.WriteLine("encrypted in {0}ms", sw.Elapsed.Milliseconds);
-                }
-
-                using (FileStream inFile = File.OpenRead(encrypted))
-                using (FileStream outFile = File.Create(fi.FullName + ".gz"))
-                using (GZipStream Compress = new GZipStream(outFile, CompressionMode.Compress))
-                {
-                    byte[] buffer = new byte[65536];
-                    int numRead;
-                    while ((numRead = inFile.Read(buffer, 0, buffer.Length)) != 0)
-                    {
-                        Compress.Write(buffer, 0, numRead);
-                    }
-                }
-
-                sw.Reset();
                 sw.Start();
 
+                // Create a decrytor to perform the stream transform.
+                ICryptoTransform encryptor = rijAlg.CreateEncryptor(rijAlg.Key, rijAlg.IV);
+
+                using (var inputFileStream = File.OpenRead(input))
+                using (var outputFileStream = File.OpenWrite(output))
+                using (var csEncrypt = new CryptoStream(outputFileStream, encryptor, CryptoStreamMode.Write))
+                using (var swEncrypt = new StreamWriter(csEncrypt))
+                {
+                    var buf = new byte[bufSize];
+
+                    int length = 0;
+                    int offset = 0;
+                    int loopCnt = 0;
+
+                    do
+                    {
+                        length = inputFileStream.Read(buf, 0, buf.Length);
+                        //Write all data to the stream.
+                        csEncrypt.Write(buf, 0, length);
+                        offset += length;
+                        loopCnt++;
+                    } while (length > 0);
+                }
+
+                sw.Stop();
+                Console.WriteLine("encrypted in {0}ms", sw.Elapsed.Milliseconds);
+            }
+
+        }
+
+        public static void DecryptFile(string input, string output)
+        {
+
+            // Create an RijndaelManaged object 
+            // with the specified key and IV. 
+            using (RijndaelManaged rijAlg = new RijndaelManaged())
+            {
+                var key = "blah";
+                rijAlg.Key = MakeKey(key);
+                rijAlg.IV = IV64Bytes;
+
+                var sw = new Stopwatch();
                 // Create a decrytor to perform the stream transform.
                 ICryptoTransform decryptor = rijAlg.CreateDecryptor(rijAlg.Key, rijAlg.IV);
 
                 // Create the streams used for decryption. 
-                using (var msDecrypt = File.OpenRead(decompressed))
-                using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
-                using (var decryptedFile = File.OpenWrite(decrypted))
+                using (var msDecrypt = File.OpenRead(input))
+                using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                using (var decryptedFile = File.OpenWrite(output))
                 {
                     var buf = new byte[bufSize];
 
@@ -124,16 +163,11 @@ namespace EncryptFile
                         offset += length;
                         loopCnt++;
                     } while (length > 0);
-
                 }
 
                 sw.Stop();
                 Console.WriteLine("decrypted in {0}ms", sw.Elapsed.Milliseconds);
             }
-
-            Console.WriteLine("press enter to exit");
-            Console.ReadLine();
-
         }
 
         private static void GenerateRandomFile(string filePath, int size)
